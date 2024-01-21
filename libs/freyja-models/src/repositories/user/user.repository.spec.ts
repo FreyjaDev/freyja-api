1daa12ba836289a4743a0f2f1a3d0ad011587243
Lazialize <lazinelia@gmail.com>
2024-01-21 18:35:31 +0900

    test: user repository test

--- libs/freyja-models/src/repositories/user/user.repository.spec.ts
+++ libs/freyja-models/src/repositories/user/user.repository.spec.ts
@@ -1,5 +1,8 @@
+import { SnowflakeId, ULID } from '@freyja-models/freyja-models';
+import User from '@freyja-models/freyja-models/entities/user';
 import { FreyjaModelsModule } from '@freyja-models/freyja-models/freyja-models.module';
 import { Test, TestingModule } from '@nestjs/testing';
+import { ulid } from 'ulidx';
 
 import { UserRepository } from './user.repository';
 
@@ -18,5 +21,21 @@ describe('UserRepository', () => {
     expect(service).toBeDefined();
   });
 
-  it('should be created a new entity', () => {});
+  it('should be created a new entity', async () => {
+    // Create a user.
+    const userId = new ULID(ulid());
+    const userSnowflakeId = new SnowflakeId('123456789012345678');
+
+    const user = User.create({
+      discordId: userSnowflakeId.value,
+      id: userId.value,
+    });
+    await service.save(user);
+
+    const fetchedUser = await service.findBySnowflakeId(userSnowflakeId);
+
+    expect(fetchedUser).not.toEqual(undefined);
+    expect(fetchedUser?.id).toEqual(userId);
+    expect(fetchedUser?.discordId).toEqual(userSnowflakeId);
+  });
 });
