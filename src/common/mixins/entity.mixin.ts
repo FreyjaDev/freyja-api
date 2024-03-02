50317f534243ced0da713b69e50b8ca2af4b454a
Lazialize <lazinelia@gmail.com>
2024-03-03 02:12:04 +0900

    feat: Refactor an entity implementation and Implement Json conversion

--- src/common/mixins/entity.mixin.ts
+++ src/common/mixins/entity.mixin.ts
@@ -1,13 +1,25 @@
 import { Cacheable } from './cacheable.mixin';
 
-type Primitive = string | number | boolean | symbol | undefined | null | bigint;
-type PropTypes = Primitive | Date;
-type Prop<T> = Partial<Record<keyof T, PropTypes>>;
+type Primitive = string | number | boolean | undefined | null | bigint;
+type PropTypes = Primitive | Date | Array<PropTypes>;
+type Prop<T> = { [P in keyof T]: PropTypes };
+type Props<T> = Prop<T> | (Prop<T> & { [P in keyof T]: Entity<Prop<T>> });
 
-export abstract class Entity<
-  T extends Prop<T> | (Prop<T> & Record<keyof T, Entity<Prop<T>>>),
-> extends Cacheable<T> {
-  protected constructor(protected props: T) {
+type JsonValue =
+  | string
+  | number
+  | bigint
+  | boolean
+  | null
+  | JsonArray
+  | JsonObject;
+interface JsonObject {
+  [key: string]: JsonValue;
+}
+interface JsonArray extends Array<JsonValue> {}
+
+export abstract class Entity<T extends Props<T>> extends Cacheable<T> {
+  constructor(protected props: T) {
     super();
   }
 
@@ -32,4 +44,32 @@ export abstract class Entity<
     }
     return true;
   }
+
+  toJSON(): JsonValue {
+    return convert(this.props);
+  }
 }
+
+const convert = <T extends Props<T>>(props: T): JsonValue => {
+  const result: JsonValue = {};
+  for (const key of Object.keys(props) as (keyof T)[]) {
+    const value = props[key];
+    if (value instanceof Entity) {
+      result[key as string] = value.toJSON();
+    } else {
+      result[key as string] = convertPropType(value);
+    }
+  }
+  return result;
+};
+
+const convertPropType = (prop: PropTypes): JsonValue => {
+  if (prop instanceof Date) {
+    return prop.toISOString();
+  } else if (Array.isArray(prop)) {
+    return prop.map((p) => convertPropType(p));
+  } else if (prop === undefined) {
+    return null;
+  }
+  return prop;
+};
