e5b1c215bec588e73392987fd98415126f30e50f
Lazialize <lazinelia@gmail.com>
2024-03-02 22:12:26 +0900

    feat: Create API endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Elysia } from 'elysia';
 
 const app = new Elysia()
+  .post('/guilds/:guildId/users', () => {})
+  .get('/guilds/:guildId/users/@me', () => {})
   .get('/guilds/:guildId/users/:userId', () => {})
-  .get('/', () => 'Hello Elysia')
+  .get('/guilds/:guildId/users/@me/games', () => {})
+  .get('/guilds/:guildId/users/:userId/games', () => {})
+  .get('/guilds/:guildId/games', () => {})
+  .post('/guilds/:guildId/games', () => {})
+  .get('/guilds/:guildId/leaderboard', () => {})
   .listen(3000);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
