4baa118590f83f1adc26665b31cfaa41abef87aa
Lazialize <lazinelia@gmail.com>
2024-03-09 02:54:26 +0900

    fix: Wrong environment variable name

--- src/common/database.ts
+++ src/common/database.ts
@@ -8,7 +8,7 @@ const userName = process.env.POSTGRES_USER;
 const password = process.env.POSTGRES_PASSWORD;
 const host = process.env.POSTGRES_HOST;
 const port = process.env.POSTGRES_PORT;
-const database = process.env.POSTGRES_NAME;
+const database = process.env.POSTGRES_DB;
 
 const connectionUrl = `postgres://${userName}:${password}@${host}:${port}/${database}`;
 const queryClient = postgres(connectionUrl);
